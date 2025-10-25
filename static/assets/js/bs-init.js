1b9066a0a7f35fd35a0820853b49cea6a4cf149b
Ömer R. Sak <orsmes6145@gmail.com>
2025-10-25 18:44:10 +0200

    some prettier updates

--- static/assets/js/bs-init.js
+++ static/assets/js/bs-init.js
@@ -1,7 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
-	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-	  return new bootstrap.Tooltip(tooltipTriggerEl);
-	})
-}, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var tooltipTriggerList = [].slice.call(
+      document.querySelectorAll("[data-bss-tooltip]"),
+    );
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  },
+  false,
+);
