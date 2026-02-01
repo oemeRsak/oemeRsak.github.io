9785946baa84b81c891c2da1d0be82cb95741826
Ömer R. Sak <orsmes6145@gmail.com>
2026-02-01 23:44:29 +0100

    remove docs link from stadtbank project

--- static/assets/js/bs-init.js
+++ static/assets/js/bs-init.js
@@ -1,3 +1,4 @@
+
 if (window.innerWidth < 768) {
 	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
 		elem.classList.remove('animated');
@@ -8,16 +9,16 @@ if (window.innerWidth < 768) {
 	});
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
 	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
 	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
-		hoverAnimationEl.addEventListener('mouseenter', function (e) { e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
-		hoverAnimationEl.addEventListener('mouseleave', function (e) { e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
 	});
 
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-		return new bootstrap.Tooltip(tooltipTriggerEl);
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
 	})
 }, false);
